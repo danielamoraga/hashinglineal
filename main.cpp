133c83ac34193e87ddc67f7edf67674bdd8889d6
elegoose <fel.torra@gmail.com>
2024-10-07 03:49:24 -0300

    formatted code & deleted unused cout comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "structures.cpp"
 #include <chrono>
 #include <fstream>
 
+#include "structures.cpp"
+
 // Función para generar un número aleatorio de 64 bits
 element numero_aleatorio_64bits() {
-    return (((element) rand() << 32) | rand());  // Combina dos llamadas de rand() para generar 64 bits
+    return (((element)rand() << 32) | rand());  // Combina dos llamadas de rand() para generar 64 bits
 }
 
 // Función para generar una secuencia de 'N' números aleatorios de 64 bits
@@ -23,20 +24,19 @@ int main() {
     // generar secuencia de N números de 64 bits |N| pertenece a {2^10, 2^11, 2^12,...,2^24}
     srand(time(0));
     for (int i = 10; i <= 24; ++i) {
-
+        cout << "Tests para N = 2^" << i << endl;
         uint N = 1 << i;
         vector<element> secuencia = generar_secuencia(N);
-        cout << "Generando secuencia de tamaño " << N << endl;
 
         for (int c = 1; c <= 10; c++) {
-        
-            HashTable H; // crear tabla de hashing con espacio inicial 1
+            cout << "Test para c = " << c << endl;
+            HashTable H;  // crear tabla de hashing con espacio inicial 1
 
             H.c_max = c;
 
             int inserciones = 0;
             int ios = 0;
-            auto start = chrono::high_resolution_clock::now(); // cronómetro
+            auto start = chrono::high_resolution_clock::now();  // cronómetro
 
             // insertar cada número en la tabla de hash
             for (element y : secuencia) {
@@ -45,44 +45,17 @@ int main() {
                 ios += H.accesses;
             }
 
-            auto end = chrono::high_resolution_clock::now(); // terminar cronómetro
+            auto end = chrono::high_resolution_clock::now();  // terminar cronómetro
             chrono::duration<double> tiempo = end - start;
 
             double costo_promedio = static_cast<double>(ios) / inserciones;
             double llenado = H.porcentaje_llenado();
 
-            cout << "Resultados para  N = 2^" << i << ":" << endl;
-            cout << "c utilizado: " << c << endl;
-            cout << "Cantidad de I/Os: " << ios << endl;
-            cout << "Costo promedio de inserción (I/Os): " << costo_promedio << endl;
-            cout << "Tiempo total de inserción: " << tiempo.count() << " segundos" << endl;
-            cout << "Porcentaje de llenado de las páginas: " << llenado << "%" << endl;
-
             csv_file << N << "," << c << "," << costo_promedio << "," << tiempo.count() << "," << llenado << "\n";
-
             csv_file.flush();
         }
     }
     csv_file.close();
+    cout << "Tests finalizados y datos guardados." << endl;
     return 0;
 }
-
-/*int main() {
-    HashTable H;
-
-    H.c_max = 1;
-
-    cout << "Espacio en la tabla: " << H.table.size() << " ? " << H.p << endl;
-    cout << "Insertando 1 elemento" << endl;
-    H.insert(675);
-    cout << "Accesos realizados: " << H.accesses << endl;
-    cout << "Espacio en la tabla: " << H.table.size() << " ? " << H.p << endl;
-    cout << "------------------------" << endl;
-    cout << "Insertando otro elemento" << endl;
-    H.insert(263);
-    cout << "------------------------" << endl;
-    cout << "Insertando otro elemento" << endl;
-    H.insert(263);
-
-    return 0;
-}*/
--- structures.cpp
+++ structures.cpp
@@ -1,14 +1,14 @@
+#include <climits>
 #include <iostream>
 #include <random>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
 #define uint unsigned long long
 #define element long long
 
-int MAX_PAGE_SIZE = 128; // cantidad máxima de elementos en una página
+int MAX_PAGE_SIZE = 128;  // cantidad máxima de elementos en una página
 
 /* Función de hashing: devuelve valor aleatorio entre 0 y 2^64 - 1 para cualquier y */
 uint h(element y) {
@@ -19,17 +19,16 @@ uint h(element y) {
 
 /* Página: Lista de elementos que tiene como máximo 1024 bytes */
 struct Page {
-    vector<element> elements; // lista de elementos de 64 bits
-    int n = 0; // cantidad de elementos en la página
-    Page *overflow = nullptr; // puntero a la página de desborde
-    int size = 0; // cantidad de elementos en la página y sus desbordes
+    vector<element> elements;  // lista de elementos de 64 bits
+    int n = 0;                 // cantidad de elementos en la página
+    Page* overflow = nullptr;  // puntero a la página de desborde
+    int size = 0;              // cantidad de elementos en la página y sus desbordes
     int accesses = 0;
 
     Page* search(element y) {
         Page* page = this;
         Page* prev = nullptr;
         while (page != nullptr) {
-            //cout << "Buscando elemento en la página" << endl;
             accesses++;
             for (element e : elements) {
                 if (e == y) {
@@ -39,8 +38,6 @@ struct Page {
             }
             prev = page;
             page = page->overflow;
-            //if (page != nullptr) cout << "Buscando en desborde" << endl;
-            //else cout << "No hay más desbordes donde buscar" << endl;
         }
         return prev;
     }
@@ -51,12 +48,12 @@ struct Page {
         if (n + 1 <= MAX_PAGE_SIZE) {
             elements.push_back(y);
             n++;
-        } else {
+        }
+        else {
             // si no hay espacio, se inserta en la página de desborde
             if (overflow == nullptr) {
                 overflow = new Page();
             }
-            //cout << "Insertando elemento en desborde" << endl;
             overflow->insert(y);
         }
     }
@@ -82,79 +79,56 @@ struct Page {
 
 /* Tabla de hashing: Lista indexada por el valor entregado por la función de hash */
 struct HashTable {
-    vector<Page*> table = {new Page()}; // vector de listas de páginas (donde las listas de rebalse son páginas con un puntero a su rebalse)
+    vector<Page*> table = {new Page()};  // vector de listas de páginas (donde las listas de rebalse son páginas con un puntero a su rebalse)
     int p = 1;
     int t = 0;
     int c_max;
     int accesses = 0;
 
     void insert(element y) {
-
         int k = h(y) % (1 << (t + 1));
-        //cout << "Hash del elemento: " << h(y) << endl;
 
         if (k < p) {
-            //cout << "Insertando en la página " << h(y) << " mod 2^(" << t << " + 1) = " << k << endl;
             Page* searched = table[k]->search(y);
             if (searched == nullptr) {
-                //cout << "Elemento ya existe" << endl;
                 return;
-            } else {
-                //cout << "Elemento no encontrado. Se inserta." << endl;
+            }
+            else {
                 searched->insert(y);
             }
-        } else {
+        }
+        else {
             k = k - (1 << t);
-            //cout << "Insertando en la página " << h(y) << " mod 2^(" << t << " + 1) - 2^" << t << " = " << k << endl;
             Page* searched = table[k]->search(y);
             if (searched == nullptr) {
-                //cout << "Elemento ya existe" << endl;
                 return;
-            } else {
-                //cout << "Elemento no encontrado. Se inserta." << endl;
+            }
+            else {
                 searched->insert(y);
             }
         }
 
         accesses = table[k]->accesses;
         if (accesses > c_max) {
-            //cout << "Se excedió el límite de accesos: accesos = " << accesses  << " > " << c_max << endl;
             expand();
         }
     }
 
     void expand() {
-        //cout << "Expandiendo la tabla" << endl;
-
-        //cout << "Antiguo tamaño: ¿" << table.size() << " = " << p << "?" << endl;
-
-        table.push_back(new Page()); // página p
-
-        //cout << "Nuevo tamaño: ¿" << table.size() << " = " << p + 1 << "?" << endl;   
+        table.push_back(new Page());  // página p
 
         int i = p - (1 << t);
 
-        //cout << "Índice de la página a redistribuir: " << i << endl;
-
-        //cout << "Página " << i << " tiene " << table[i]->get_elements().size() << " elementos" << endl;
-        
-        vector <element> redistribute = table[i]->get_elements();
+        vector<element> redistribute = table[i]->get_elements();
         table[i] = new Page();
 
-        //cout << "Página " << i << " vaciada, ahora tiene " << table[i]->get_elements().size() << " elementos" << endl;
-        //cout << "Elementos a redistribuir: " << redistribute.size() << endl;
-
         for (element y : redistribute) {
-            
-            //cout << "Redistribuyendo elemento " << y << endl;
-            
             int k = h(y) % (1 << (t + 1));
 
             if (k == p) {
-                //cout << "Insertando en la página " << p << endl;
                 table[p]->insert(y);
-            } else {
-                //cout << "Insertando en la página " << i << endl;
+            }
+            else {
                 table[i]->insert(y);
             }
         }
@@ -162,9 +136,8 @@ struct HashTable {
         // compactar páginas de desborde
 
         p++;
-        if (p == (1 << (t + 1))) t++;
-
-        //cout << "Tabla expandida. Nuevo tamaño: " << p << endl;
+        if (p == (1 << (t + 1)))
+            t++;
     }
 
     double porcentaje_llenado() const {
@@ -181,7 +154,8 @@ struct HashTable {
                 }
             }
         }
-        if (total == 0) return 0.0;
-        return (double) ocupado / (total * MAX_PAGE_SIZE) * 100;
+        if (total == 0)
+            return 0.0;
+        return (double)ocupado / (total * MAX_PAGE_SIZE) * 100;
     }
 };
